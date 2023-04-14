018815e7340ef236ce6f0da5ffacb0013cb5d26e
XiangrongXu <xiangrong_xxr@outlook.com>
2023-04-14 21:34:02 +0800

    update the version of mybatis plus

--- src/main/java/net/revcn/dao/MetricDao.java
+++ src/main/java/net/revcn/dao/MetricDao.java
@@ -1,12 +1,9 @@
 package net.revcn.dao;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
-
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import net.revcn.domain.Metric;
 
 @Mapper
-@Component
 public interface MetricDao extends BaseMapper<Metric> {
 }
--- src/main/java/net/revcn/dao/UserDao.java
+++ src/main/java/net/revcn/dao/UserDao.java
@@ -1,9 +1,6 @@
 package net.revcn.dao;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.stereotype.Component;
-
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import net.revcn.domain.User;
 
