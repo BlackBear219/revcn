8b450d93aa41b46829487b737f2f93b561f78412
XiangrongXu <xiangrong_xxr@outlook.com>
2023-04-17 10:57:19 +0800

    temp commit

--- src/main/java/net/revcn/service/MetricService.java
+++ src/main/java/net/revcn/service/MetricService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.revcn.dao.MetricDao;
-
 import net.revcn.domain.Metric;
 import net.revcn.model.MetricType;
 
